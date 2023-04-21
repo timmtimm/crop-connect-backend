1fda73702b3ad01c4896cdd81127004c336cac90
timmtimm <timotiuswirawan38@gmail.com>
2023-04-21 15:46:59 +0700

    style: seeder folder structure

--- seeds/seeder.go
+++ seeds/seeder.go
@@ -1,16 +1,17 @@
 package seeds
 
 import (
-	"crop_connect/business/regions"
+	regionDomain "crop_connect/business/regions"
 	_mongo "crop_connect/driver/mongo"
+	regionSeed "crop_connect/seeds/regions"
 	"fmt"
 
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func SeedDatabase(db *mongo.Database, regionUC regions.UseCase) {
+func SeedDatabase(db *mongo.Database, regionUC regionDomain.UseCase) {
 	if check, _ := _mongo.CheckCollectionExist(db, "regions"); !check {
 		fmt.Println("Seeding regions...")
-		SeedRegion(regionUC)
+		regionSeed.Seed(regionUC)
 	}
 }
