403bf3abc0aae00cb212dbe2e329c132a901867a
timmtimm <timotiuswirawan38@gmail.com>
2023-06-29 01:27:39 +0700

    fix: date time not sync with local server

--- business/treatment_records/usecase.go
+++ business/treatment_records/usecase.go
@@ -208,7 +208,7 @@ func (tru *TreatmentRecordUseCase) FillTreatmentRecord(domain *Domain, farmerID 
 		return Domain{}, statusCode, err
 	}
 
-	if treatmentRecord.Date > primitive.NewDateTimeFromTime(time.Now()) {
+	if treatmentRecord.Date > primitive.NewDateTimeFromTime(time.Now().Add(7*time.Hour)) {
 		return Domain{}, http.StatusBadRequest, errors.New("riwayat perawatan belum bisa diisi")
 	}
 
